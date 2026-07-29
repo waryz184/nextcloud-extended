b13becedd318f25151c46c445042e67d33d7f294
waryz184 <noe.hcd@gmail.com>
2026-07-29 16:43:47 +0200

    chore: bump version to 2.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "xyz.luna.nextcloudextended"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.3.3"
+        versionCode = 23
+        versionName = "2.3.4"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
